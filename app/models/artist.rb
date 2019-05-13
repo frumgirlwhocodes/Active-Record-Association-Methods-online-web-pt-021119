0f44c4250902af3f44074a5f124c502665270242
lieby weintraub <liebahinda@gmail.com>
2019-05-13 03:04:00 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,8 +6,7 @@ class Artist < ActiveRecord::Base
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
     self.genres.first
-    binding.pry
-  end
+   end
 
   def song_count
     #return the number of songs associated with the artist
